4ebe687d45af70d7f17172b8b4efbc0a707060a4
kow <kow@xploit.kr>
2020-06-16 13:21:09 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='romanizer',
+    version='1.0',
+    description='Rominize Hangul strings.',
+    long_description=get_readme(),
+    packages=(
+        'romanizer',
+    )
+)
